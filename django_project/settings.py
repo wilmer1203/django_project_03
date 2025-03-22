705cad8f07813e707c6790656a4ed54e9f1a0ca2
Wilmer <wilmeraguirre2002.3@gmail.com>
2025-03-22 10:08:43 -0400

    css post

--- django_project/settings.py
+++ django_project/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
-# STATICFILES_DIRS = [BASE_DIR / 'static'] 
+STATICFILES_DIRS = [BASE_DIR / 'static'] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
